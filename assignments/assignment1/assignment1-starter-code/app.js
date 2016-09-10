5fcc8a4be2173db92d4da6c91620c4b8eac6ae4e
Leonel Atencio <leo.atencio@gmail.com>
2016-09-09 21:03:32 -0500

    first attempt

---
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('CheckDishesController', function ($scope) {
+  $scope.output = "";
+  $scope.dishes = "";
+
+  $scope.checkIfTooMuch = function () {
+    var output = calculateOutput($scope.dishes);
+    $scope.output = output;
+  };
+
+
+  function calculatNumericForString(string) {
+    string = string.trim();
+
+    if (string == "") {
+      return "Please enter data first"
+    }
+
+    dish_list = string.split(",")
+    if (dish_list.length > 3) {
+      return "Too much!"
+    } else {
+      return "Enjoy!"
+    }
+
+    return totalStringValue;
+  }
+
+});
+
+})();
